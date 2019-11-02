80d8787c757c5ff6fca83c3ab64262d7463e0374
rodney <rodney@rodneykahan.es>
2019-11-02 14:04:44 -0400

    initial commit

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    [SerializeField] Transform target;    //player object
+    
+    void Update()
+    {
+
+        transform.position = target.position;
+
+    }
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            MoveToCursor();  
+
+        }
+        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        //lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            Debug.Log("we have hit");
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}
