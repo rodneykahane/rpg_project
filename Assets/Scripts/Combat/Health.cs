ab45ed03b3bdd443b99c512ea20d2f8757cd7999
rodney <rodney@rodneykahan.es>
2019-11-06 21:15:41 -0500

    death animation working

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -5,12 +5,28 @@ namespace RPG.Combat
 
     public class Health : MonoBehaviour
     {
-        [SerializeField] float health = 100f;
+        [SerializeField] float healthPoints = 100f;
+        bool isDead = false;
 
         public void TakeDamage(float damage)
         {
-            health = Mathf.Max(health - damage, 0);
-            Debug.Log(name + "'s health is: " + health);
+            if (healthPoints > 0)
+            {
+                healthPoints = Mathf.Max(healthPoints - damage, 0);
+                Debug.Log(name + "'s health is: " + healthPoints);
+            }
+            else if (healthPoints <= 0)
+            {
+                Die();
+                
+            }
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");            
         }
     }
 }
