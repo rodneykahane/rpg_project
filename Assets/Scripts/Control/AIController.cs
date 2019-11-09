9dee217a3e33d0df2ea7502f2f366a2cd96bb1b4
rodney <rodney@rodneykahan.es>
2019-11-09 15:20:27 -0500

    adding enemy ai

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+        
+
+        private void Update()
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            DistanceToPlayer(player);
+        }
+
+        private void DistanceToPlayer(GameObject player)
+        {
+            if (Vector3.Distance(this.transform.position, player.transform.position) < chaseDistance)
+            {
+                Debug.Log(name + " is chasing the Player!");
+            }
+        }
+    }
+}
