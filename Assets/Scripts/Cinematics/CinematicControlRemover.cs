d55ade0c125bc43dc846d961ffb7cf065a87eea4
rodney <rodney@rodneykahan.es>
2019-11-22 11:58:54 -0500

    added a game moment

---
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
+
+namespace RPG.Cinematics
+{
+    public class CinematicControlRemover : MonoBehaviour
+    {
+        GameObject player;
+
+        private void Start()
+        {
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        void DisableControl(PlayableDirector director)
+        {
+            Debug.Log("DisableControl");
+            
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
+        }
+
+        void EnableControl(PlayableDirector director)
+        {
+            
+            Debug.Log("EnableControl");
+            player.GetComponent<PlayerController>().enabled = true;
+
+        }
+    }
+}
