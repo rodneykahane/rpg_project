cf938bd5e77e3b2dcfdaa912ed0169d20ff42018
rodney <rodney@rodneykahan.es>
2019-11-25 11:36:25 -0500

    added "portals" to change scenes

---
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+
+    [SerializeField] int sceneToLoad = -1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
